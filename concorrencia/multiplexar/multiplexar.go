a693051443ffb83f3d3021f571093b8d1d7771a6
Paulo Azevedo <paulo.azevedo@mercadobitcoin.com.br>
2022-08-13 17:07:12 -0300

    1st Commit

--- concorrencia/multiplexar/multiplexar.go
+++ concorrencia/multiplexar/multiplexar.go
@@ -21,10 +21,9 @@ func juntar(entrada1, entrada2 <-chan string) <-chan string {
 
 func main() {
 	c := juntar{
-		html
-			.Titulo("https://www.cod3r.com.br", "https://www.google.com"),
+		html.Titulo("https://www.cod3r.com.br", "https://www.google.com"),
 		html.Titulo("https://www.amazon.com", "https://www.youtube.com"),
-	)
+	}
 	fmt.Println(<-c, "|", <-c)
 	fmt.Println(<-c, "|", <-c)
 }
